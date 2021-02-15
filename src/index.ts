a70fa46e6be0e918dd8dc3771b2372456baa20b2
SomberTM <sombertm@gmail.com>
2021-02-14 19:26:49 -0500

    Upload

--- src/index.ts
+++ src/index.ts
@@ -16,66 +16,6 @@ import { Color, Colors } from './utils/Color';
 
 (async function() {
 
-    // -- Json tests
 
-    // const mirror = Json.mirrorFormated([ "rateLimit", "guildCreated" ], (e: string) => { return Case.snake(e).toUpperCase(); });
-    // console.log(mirror);
-
-    // const json = { rateLimit: 'rateLimit' };
-    // console.log(json);
-
-    // const formatted = Json.formatKeys(json, (e: string) => { return Case.snake(e).toUpperCase() });
-    // console.log(formatted);
-    
-    // const foo = { hello: 'world' };
-    // const bar = { complex: { type: foo } };
-    // const x = Json.copy(bar);
-    // console.log(x.complex.type.hello);
-
-    // const baseOptions = { ttl: 0 };
-    // const providedOptions = { ttl: 5, iterable: ["a", "b"] };
-    // console.log(Json.merge(baseOptions, providedOptions));
-
-    // -- Discord Tests
-
-    //const intents = new Intents([ 'GUILDS', 'GUILD_BANS' ]);
-    //console.log(intents.has('DIRECT_MESSAGES'));
-
-    // const intents = new Intents([ 'GUILDS', 'GUILD_BANS' ]);
-    // console.log(intents.missing());
-
-    const client: Client = new Client();
-
-    await client.login('NzUyMjQ0MDM1NTgxNjQwNzI1.X1Uz2g.g-w5Y8EaSDvByCC_kSWWhu7lh8A', Intents.ALL);
-
-    const sleep = (ms: number) => {
-        return new Promise<void>((res, rej) => {
-            setTimeout(() => {
-                res();
-            }, ms);
-        })
-    }
-
-    await sleep(10000);
-
-    let guild = client.guilds.resolve("749069860050436152")!;
-
-    console.log(guild);
-
-    // let embed = new MessageEmbed().setTitle("Title").setAuthor({ name: "Author" });
-
-    // console.log(embed.toJson());
-
-    // let presence = guild.presences.find((presence: Presence) => presence.activities.length > 0);
-
-    // console.log(guild)
-    // let vs = guild.voiceStates.cache.first();
-    // console.log(vs);
-    // console.log(`'${vs?.user.username}' is connected to '${vs?.channel.name}'`)
-    // console.log(guild.channels.cache.map((value: GuildChannel) => `${value.typeString}: ${value.name}`));
-
-    // let testRole = guild?.roles.cache.find((role: Role) => role.tags != undefined);
-    // let testEmoji = guild.emojis.cache.first()!;
-    // console.log(testEmoji);
 
 })();
